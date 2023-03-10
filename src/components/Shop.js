e0c84d8d0fa5de064ce74d3b2cf11bacac6a18e2
jayu-108 <barijayu@gmail.com>
2023-03-10 11:44:11 +0530

    redux_pro_setup

---
+++ src/components/Shop.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Shop = () => {
+    return (
+        <div>
+            <h2>Buy Shoes</h2>
+            <button className="btn-primary">+</button>
+            Add To Cart
+            <button className="btn-primary">-</button>
+        </div>
+    )
+}
+
+export default Shop
