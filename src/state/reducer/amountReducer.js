fbb13db0964e172951656dca2ec2eeeaaffee5e6
jayu-108 <barijayu@gmail.com>
2023-03-10 12:33:10 +0530

    store_created

--- src/state/reducer/amountReducer.js
+++ src/state/reducer/amountReducer.js
@@ -1,10 +1,12 @@
-export default reducer = (state=0, action) =>{
-    if(action=='deposit'){
+const reducer = (state = 0, action) => {
+    if (action === 'deposit') {
         return state + action.payload
     }
-    else if(action == 'withdraw'){
+    else if (action === 'withdraw') {
         return state - action.payload
-    }else{
+    } else {
         return state
     }
 }
+
+export default reducer   
